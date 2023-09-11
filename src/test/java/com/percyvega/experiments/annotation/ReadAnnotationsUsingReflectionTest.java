5b478ff09c31f92ad730240017ff28537ffb2b6f
percyvega <percyvega@gmail.com>
2023-09-11 01:11:57 -0400

    Annotation files

---
+++ src/test/java/com/percyvega/experiments/annotation/ReadAnnotationsUsingReflectionTest.java
@@ -0,0 +1,60 @@
+package com.percyvega.experiments.annotation;
+
+import com.percyvega.experiments.annotation.custom.*;
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+
+@Log4j2
+public class ReadAnnotationsUsingReflectionTest {
+
+    List<Class<? extends Printer>> printerImplementations = Arrays.asList(DefaultPrinter.class, HtmlPrinter.class, CapitalizedPrinter.class);
+
+    @Test
+    void printPrinter() {
+        new DefaultPrinter().printPrinter("Hola");
+        new HtmlPrinter().printPrinter("Percy");
+        new CapitalizedPrinter().printPrinter("Vega");
+    }
+
+    @Test
+    void print_all_methods_and_all_their_annotations() {
+        for (Class<? extends Printer> printerImplementation : printerImplementations) {
+            log.info(printerImplementation.getSimpleName());
+            Method[] methods = printerImplementation.getMethods();
+            for (Method method : methods) {
+                log.info("\t" + method.getName());
+                Annotation[] annotations = method.getAnnotations();
+                for (Annotation annotation : annotations) {
+                    log.info("\t\t" + annotation);
+                }
+            }
+        }
+    }
+
+    @Test
+    void get_all_methods_with_a_specific_annotation_and_invoke_them() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        String singleStringParameterValue = "Percy";
+        Class<InvokableWithASingleStringParameter> annotationToLookFor = InvokableWithASingleStringParameter.class;
+
+        for (Class<? extends Printer> printerImplementation : printerImplementations) {
+            Constructor<? extends Printer> printerImplementationConstructor = printerImplementation.getDeclaredConstructor();
+            Printer printer = printerImplementationConstructor.newInstance();
+
+            Method[] methods = printerImplementation.getMethods();
+            for (Method method : methods) {
+                if (method.isAnnotationPresent(annotationToLookFor)) {
+                    method.invoke(printer, singleStringParameterValue);
+                }
+            }
+        }
+    }
+
+}
+
---
+++ src/test/java/com/percyvega/experiments/annotation/generalpurpose/ChildClass.java
@@ -0,0 +1,26 @@
+package com.percyvega.experiments.annotation.generalpurpose;
+
+import lombok.extern.log4j.Log4j2;
+
+import java.util.ArrayList;
+
+@Log4j2
+public class ChildClass extends ParentClass {
+
+    @Override
+    public void method1() {
+        log.info("ChildClass method1() running");
+    }
+
+    public static void main(String[] args) {
+
+        @SuppressWarnings("unused")
+        int a = 10;
+
+        @SuppressWarnings({"rawtypes", "unused"})
+        ArrayList arrayList = new ArrayList();
+
+        ChildClass childClass = new ChildClass();
+        childClass.method2(1);
+    }
+}
